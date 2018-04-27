1346e7cb9e3f6053fd323d5ef67374827f79e1ac
TaSiya <siyandamakhalima.1@gmail.com>
2018-04-27 19:39:40 +0200

    did few test and got stuck on testing object data

--- test/registration_number.test.js
+++ test/registration_number.test.js
@@ -20,7 +20,64 @@ describe('Number Plate Registration App', function(){
         app1.checkRegistration(app1.getNumberPlate());
         app1.setNumberPlate('CK 567');
         app1.checkRegistration(app1.getNumberPlate());
-        assert.equal()
+        assert.equal(app1.regLength(), 6)
+      });
+      it('0 correct number plates and 4 incorrect number plates', function(){
+        var app2 = NumberPlateRegister();
+
+        app2.setNumberPlate('C 2262');
+        app2.checkRegistration(app2.getNumberPlate());
+        app2.setNumberPlate('QC 2882');
+        app2.checkRegistration(app2.getNumberPlate());
+        app2.setNumberPlate('CK 1562');
+        app2.checkRegistration(app2.getNumberPlate());
+        app2.setNumberPlate('CC 2192');
+        app2.checkRegistration(app2.getNumberPlate());
+        assert.equal(app2.regLength(), 0);
+      });
+    });
+    describe('Does it only take CA, CJ, CY, and CL only? YES!',function(){
+      it('Taking CA', function(){
+        var app3 = NumberPlateRegister();
+        app3.setNumberPlate('CA 123');
+        assert.equal(app3.checkRegistration(app3.getNumberPlate()), 'CA 123');
+      });
+      it('Taking CJ', function(){
+        var app4 = NumberPlateRegister();
+        app4.setNumberPlate('CJ 123');
+        assert.equal(app4.checkRegistration(app4.getNumberPlate()), 'CJ 123');
+      });
+      it('Taking CC', function(){
+        var app5 = NumberPlateRegister();
+        app5.setNumberPlate('CC 123');
+        assert.equal(app5.checkRegistration(app5.getNumberPlate()), '');
+      });
+      it('Taking CL', function(){
+        var app6 = NumberPlateRegister();
+        app6.setNumberPlate('CL 123');
+        assert.equal(app6.checkRegistration(app6.getNumberPlate()), 'CL 123');
+      });
+      it('Taking C', function(){
+        var app7 = NumberPlateRegister();
+        app7.setNumberPlate('C 123');
+        assert.equal(app7.checkRegistration(app7.getNumberPlate()), '');
+      });
+    });
+    describe('Does it store data as object(s)', function(){
+      it('taking 3 correct plates and 2 incorrect plates',function(){
+        var app8 = NumberPlateRegister();
+        app8.setNumberPlate('CL 234');
+        app8.checkRegistration(app8.getNumberPlate());
+        app8.setNumberPlate('CA 234');
+        app8.checkRegistration(app8.getNumberPlate());
+        app8.setNumberPlate('CY 234');
+        app8.checkRegistration(app8.getNumberPlate());
+        app8.setNumberPlate('CX 234');
+        app8.checkRegistration(app8.getNumberPlate());
+        app8.setNumberPlate('CZ 234');
+        app8.checkRegistration(app8.getNumberPlate());
+        
+        assert.deepEqual(app8.getDataMap(),);
       });
     });
 });
